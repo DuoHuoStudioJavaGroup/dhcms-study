fe43bbe2b244f555fbcf118956ad705498359b17
adolphlwq <kenan3015@gmail.com>
2015-06-16 15:42:40 +0800

    edit articles with add,list,delete and change function

---
+++ src/com/dh/youth/dao/ArticleDao.java
@@ -0,0 +1,13 @@
+package com.dh.youth.dao;
+
+import java.util.ArrayList;
+
+import com.dh.youth.beans.Article;
+
+public interface ArticleDao {
+	boolean insertArticle(Article article);
+	ArrayList<Article> getArticleList();
+	Article getArticleById(int id);
+	int updateArticle(Article article);
+	int deleteArticle(int id);
+}
